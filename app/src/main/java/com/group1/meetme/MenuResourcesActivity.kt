989a095a5b5b124960a8b8280476012ff63c25b0
Payal429 <payal.sunil429@gmail.com>
2025-05-22 13:34:07 +0200

    updatng new features

--- app/src/main/java/com/group1/meetme/MenuResourcesActivity.kt
+++ app/src/main/java/com/group1/meetme/MenuResourcesActivity.kt
@@ -11,6 +11,8 @@ class MenuResourcesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_resources)
 
+
+
         // Reference to the Upload Resources card
         val uploadLayout = findViewById<LinearLayout>(R.id.uploadResourcesLayout)
 
@@ -28,44 +30,52 @@ class MenuResourcesActivity : AppCompatActivity() {
             val intent = Intent(this, DownloadResourcesActivity::class.java)
             startActivity(intent)
         }
-        val homebutton: ImageButton = findViewById(R.id.homebutton)
-        val schedulebutton: ImageButton = findViewById(R.id.schedulebutton)
-        val bookingsbutton: ImageButton = findViewById(R.id.bookingsbutton)
-        val settingsbutton: ImageButton = findViewById(R.id.settingsbutton)
-        val resourcebutton: ImageButton = findViewById(R.id.resourcesbutton)
+//        val homebutton: ImageButton = findViewById(R.id.homebutton)
+//        val schedulebutton: ImageButton = findViewById(R.id.schedulebutton)
+//        val bookingsbutton: ImageButton = findViewById(R.id.bookingsbutton)
+//        val settingsbutton: ImageButton = findViewById(R.id.settingsbutton)
+//        val resourcebutton: ImageButton = findViewById(R.id.resourcesbutton)
 
-        homebutton.setOnClickListener {
-            val sharedPref = getSharedPreferences("UserSession", MODE_PRIVATE)
-            val role = sharedPref.getString("role", "student") // default to student
+        // Return back to the dashboard
+        val backArrow: ImageButton = findViewById(R.id.backArrow)
 
-            val intent = if (role == "lecturer") {
-                Intent(this, LecturerDashboardActivity::class.java)
-            } else {
-                Intent(this, StudentDashboardActivity::class.java)
-            }
-
+        backArrow.setOnClickListener(){
+            val intent = Intent(this, StudentDashboardActivity::class.java)
             startActivity(intent)
         }
 
-        schedulebutton.setOnClickListener(){
-            val intent = Intent(this, BookAppointmentActivity::class.java)
-            startActivity(intent)
-        }
-
-        bookingsbutton.setOnClickListener(){
-            val intent = Intent(this, AppointmentsActivity::class.java)
-            startActivity(intent)
-        }
-
-        resourcebutton.setOnClickListener(){
-            val intent = Intent(this, MenuResourcesActivity::class.java)
-            startActivity(intent)
-        }
-
-
-        settingsbutton.setOnClickListener(){
-
-        }
+//        homebutton.setOnClickListener {
+//            val sharedPref = getSharedPreferences("UserSession", MODE_PRIVATE)
+//            val role = sharedPref.getString("role", "student") // default to student
+//
+//            val intent = if (role == "lecturer") {
+//                Intent(this, LecturerDashboardActivity::class.java)
+//            } else {
+//                Intent(this, StudentDashboardActivity::class.java)
+//            }
+//
+//            startActivity(intent)
+//        }
+//
+//        schedulebutton.setOnClickListener(){
+//            val intent = Intent(this, BookAppointmentActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        bookingsbutton.setOnClickListener(){
+//            val intent = Intent(this, AppointmentsActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        resourcebutton.setOnClickListener(){
+//            val intent = Intent(this, MenuResourcesActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//
+//        settingsbutton.setOnClickListener(){
+//
+//        }
 
     }
 }
